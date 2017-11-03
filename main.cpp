bd695db102a11d44af9780ce8c3f01e8bc18eca5
Matheus Soares <m.soares.ms@outlook.com>
2017-11-03 18:23:48 -0400

    classe Item

--- main.cpp
+++ main.cpp
@@ -1,30 +1,6 @@
 #include <iostream>
+#include "Item.h"
 using namespace std;
-
-class Item{
-private:
-    int chave;
-    string descricao;
-public:
-    Item(){}
-    Item(int pChave, string pDescricao){
-        this->chave = pChave;
-        this->descricao = pDescricao;
-    }
-    int getChave() const {
-        return chave;
-    }
-    void setChave(int chave) {
-        Item::chave = chave;
-    }
-    string getDescricao() const {
-        return descricao;
-    }
-    void setDescricao(string descricao) {
-        Item::descricao = descricao;
-    }
-    void mostrar();
-};
 
 template <class T>
 class No{
@@ -76,11 +52,6 @@ public:
     void enfileira(T);
     void desenfileira(T *);
 };
-
-void Item::mostrar() {
-    cout << "Descricao: " << this->descricao << endl;
-    cout << "Chave: " << this->chave << endl << endl;
-}
 
 template <typename T>
 void Fila<T>::enfileira(T pItem) {
